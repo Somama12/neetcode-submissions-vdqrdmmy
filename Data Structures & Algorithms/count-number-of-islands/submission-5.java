61f325faa68b175c32294b96b92865278931addd
Somama Siddiqui <146497258+Somama12@users.noreply.github.com>
2026-04-08 21:39:47 -0500

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+
+        int island = 0;
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(grid[i][j] == '1'){
+                    island++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return island;
+    }
+
+    public void dfs(int i, int j, char[][] grid){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(i+1, j, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
+    }
+}
